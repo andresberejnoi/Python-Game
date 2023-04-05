9650f1a202bca802b267c50c4aea5a5a14c5a558
Andres Berejnoi <andresberejnoi@gmail.com>
2023-04-05 16:32:51 -0400

    working on a camera system which follows the player and keeps it centered

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-'''Some of the code was adapted from these tutorials:
-        * https://www.youtube.com/watch?v=OAH8K5lVYOU&ab_channel=CodingWithRuss
-        * https://www.youtube.com/watch?v=sVbFS9qEl4Y&ab_channel=ScriptLineStudios
+'''
+Some of the code was adapted from these tutorials:
+    * https://www.youtube.com/watch?v=OAH8K5lVYOU&ab_channel=CodingWithRuss
+    * https://www.youtube.com/watch?v=sVbFS9qEl4Y&ab_channel=ScriptLineStudios
+
+I read the answers to the following StackOverflow question to get a better idea of how to implement a 
+camera system:
+https://stackoverflow.com/questions/67526781/trying-to-make-screen-center-to-player-in-pygame-simple-2d-platformer
 '''
 import pygame
 import sys
 
 import agents
+import objects
 
 
 pygame.init()
@@ -23,6 +29,12 @@ pygame.display.set_caption("Prototype Survival Game")
 
 #-- Define the player object
 player = agents.Player(screen=screen)
+
+#-- Random objects for testing
+objects = [objects.Rock(screen) for i in range(10)]
+
+#-- CAMERA SETUP
+camera = pygame.math.Vector2((0,0))
 
 #-- Main loop
 keep_running = True
@@ -50,9 +62,15 @@ while keep_running:
         print(chr(pygame.K_s))
 
     #-- Handle camera updates
-
+    # in the main loop: adjust the camera position to center the player
+    camera.x = player.x - SCREEN_WIDTH / 2
+    camera.y = player.y - SCREEN_HEIGHT / 2
+    
     #-- draw the playr
     player.update(screen)
+
+    for item in objects:
+        item.update(screen)
 
     pygame.display.update()
 
