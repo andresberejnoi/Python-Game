08baf1af17c81bdba243e0e0c1b6c6e469aab3a4
Andres Berejnoi <andresberejnoi@gmail.com>
2023-04-06 16:26:25 -0400

    Completely changed file after following some tutorials on camera control

--- main.py
+++ main.py
@@ -1,44 +1,101 @@
-'''
-Some of the code was adapted from these tutorials:
-    * https://www.youtube.com/watch?v=OAH8K5lVYOU&ab_channel=CodingWithRuss
-    * https://www.youtube.com/watch?v=sVbFS9qEl4Y&ab_channel=ScriptLineStudios
-
-I read the answers to the following StackOverflow question to get a better idea of how to implement a 
-camera system:
-https://stackoverflow.com/questions/67526781/trying-to-make-screen-center-to-player-in-pygame-simple-2d-platformer
-'''
 import pygame
-import sys
-
 import agents
 import objects
+import spritesheet
+import random
+import os
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, group, image) -> None:
+        super().__init__(group)
+        self.pos = pos
+        self.image = image
+
+        self.rect = self.image.get_rect(center=pos)
+        self.direction = pygame.math.Vector2()
+        self.speed = 3
+
+    def input(self):
+        keys_pressed = pygame.key.get_pressed()
+
+        if keys_pressed[pygame.K_a]:
+            self.direction.x = -1
+        elif keys_pressed[pygame.K_d]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+        if keys_pressed[pygame.K_w]:
+            self.direction.y = -1
+        elif keys_pressed[pygame.K_s]:
+            self.direction.y = 1 
+        else:
+            self.direction.y = 0
+
+    def update(self):
+        self.input()
+        self.rect.center += self.direction * self.speed
+
+
+class Rock(pygame.sprite.Sprite):
+    def __init__(self, pos, group, image):
+        super().__init__(group)
+        self.pos = pos
+
+        #self.image = pygame.image.load(os.path.join(graphics_folder, "rock.png")).convert_alpha()
+        self.image = image
+        self.rect =  self.image.get_rect(topleft = pos)
+
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+    
+    def custom_draw(self,):
+        for sprite in self.sprites():
+            self.display_surface.blit(sprite.image, sprite.rect) 
+
+#=======================
+WHITE = (255, 255, 255)
+BLACK = (  0,   0,   0)
+
+#=======================
+graphics_folder = "sprites"
 pygame.init()
-
 clock = pygame.time.Clock()
 FPS   = 60
 
-
-#-- Define Window Size and Create Screen
+#========================
+#
 SCREEN_WIDTH  = 960
 SCREEN_HEIGHT = 540 
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Prototype Survival Game")
 
-#-- Define the player object
-player = agents.Player(screen=screen)
+#========================
+#-- Load Spritesheet
+_player_spritesheet_img  = pygame.image.load(os.path.join(graphics_folder, "human_regular_hair.png")).convert_alpha()
+_rock_spritesheet_img    = pygame.image.load(os.path.join(graphics_folder,"rock.png")).convert_alpha()
 
-#-- Random objects for testing
-objects = [objects.Rock(screen) for i in range(10)]
+player_spritesheet = spritesheet.SpriteSheet(_player_spritesheet_img, single_width=20, single_height=20)
+rock_spritesheet   = spritesheet.SpriteSheet(_rock_spritesheet_img, single_width=25, single_height=25)
 
-#-- CAMERA SETUP
-camera = pygame.math.Vector2((0,0))
+#
+camera_group = CameraGroup()
 
-#-- Main loop
+player_sprite = player_spritesheet.get_image((0,0), scale=3)
+rock_sprite   = rock_spritesheet.get_image((0,0), scale=3, chromakey=WHITE)
+player = Player((0,0), camera_group, image=player_sprite)
+
+for i in range(20):
+    rand_x = random.randint(0,1300)
+    rand_y = random.randint(0,1300)
+    Rock((rand_x, rand_y), camera_group, image=rock_sprite)
+
+#========================
+# MAIN LOOP
 keep_running = True
-
 while keep_running:
     screen.fill((100,200,100))
     clock.tick(FPS)
@@ -47,30 +104,11 @@ while keep_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             keep_running = False
-            #sys.quit()
 
-    #-- collect pressed keys and check if motion was used
-    keys_pressed = pygame.key.get_pressed()
+    #screen.blit(player_sprite, (0,0))
 
-    if keys_pressed[pygame.K_a]:
-        print(chr(pygame.K_a)) 
-    if keys_pressed[pygame.K_d]:
-        print(chr(pygame.K_d)) 
-    if keys_pressed[pygame.K_w]:
-        print(chr(pygame.K_w)) 
-    if keys_pressed[pygame.K_s]:
-        print(chr(pygame.K_s))
-
-    #-- Handle camera updates
-    # in the main loop: adjust the camera position to center the player
-    camera.x = player.x - SCREEN_WIDTH / 2
-    camera.y = player.y - SCREEN_HEIGHT / 2
-    
-    #-- draw the playr
-    player.update(screen)
-
-    for item in objects:
-        item.update(screen)
+    camera_group.update()
+    camera_group.custom_draw()
 
     pygame.display.update()
 
