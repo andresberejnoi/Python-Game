e4f7194762c03eb79a8e6fc194a7e906faa0e58a
Andres Berejnoi <andresberejnoi@gmail.com>
2023-04-10 21:45:52 -0400

    added convenience function to return a list with all sprites in the sheet (based on given dimensions)

--- spritesheet.py
+++ spritesheet.py
@@ -7,10 +7,13 @@ I followed a tutorial used for this video:
 import pygame
 
 class SpriteSheet():
-    def __init__(self, image, single_width=24, single_height=24):
+    def __init__(self, image, single_width=24, single_height=24, num_cols=8, num_rows=1):
         self.sheet = image
         self.single_width  = single_width
         self.single_height = single_height
+
+        self.num_cols = num_cols
+        self.num_rows = num_rows
 
     @staticmethod
     def load_from_file(spritesheet_file, single_width=24, single_height=24):
@@ -36,3 +39,13 @@ class SpriteSheet():
         img.set_colorkey(chromakey)
 
         return img
+    
+    def get_all_sprites(self, scale=1) -> list:
+        """Returns a list of all sprites in spritesheet, based on self.num_cols x self.num_rows"""
+        sprites_list = []
+        for col in range(self.num_cols):
+            for row in range(self.num_rows):
+                img = self.get_image((col,row), scale=scale,)
+                sprites_list.append(img)
+
+        return sprites_list
