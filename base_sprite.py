5bae35b6844cf9cd364da654b25092de4eb31127
Andres Berejnoi <andresberejnoi@gmail.com>
2023-04-14 14:05:23 -0400

    added ability to control scale and chromakey values by passing arguments to class creator

--- base_sprite.py
+++ base_sprite.py
@@ -1,4 +1,5 @@
 import pygame
+from config import *
 
 class PrimordialSprite(pygame.sprite.Sprite):
     def __init__(self, 
@@ -8,12 +9,15 @@ class PrimordialSprite(pygame.sprite.Sprite):
                  spritesheet=None, 
                  animation_speed=None, 
                  can_collide=True,
-                 hitbox_shrink_factor:int | tuple |list = (0.30, 0.85)):
+                 hitbox_shrink_factor:int | tuple |list = (0.30, 0.85),
+                 sprite_scale = 1,
+                 **kwargs):
         super().__init__(groups)
 
+        self._sprite_chromakey  = kwargs.get('sprite_chromakey', BLACK)
         if image is None:
             if spritesheet is not None:
-                self.image = spritesheet.get_image((0,0), scale=3)
+                self.image = spritesheet.get_image((0,0), scale=sprite_scale, chromakey=self._sprite_chromakey)
             else:
                 raise #placeholder until I decide best course of action here
         else:
@@ -22,7 +26,6 @@ class PrimordialSprite(pygame.sprite.Sprite):
 
         #-- Rectangle and position elements
         self.rect = self.image.get_rect(center = pos)
-        #self.rect = self.image.get_rect(topleft = pos)
         self.pos  = pygame.math.Vector2(self.rect.center)
 
         #-- Collision elements
@@ -32,9 +35,6 @@ class PrimordialSprite(pygame.sprite.Sprite):
         else:
             self._hitbox_shrink_x_factor = hitbox_shrink_factor[0]
             self._hitbox_shrink_y_factor = hitbox_shrink_factor[1]
-
-        print(hitbox_shrink_factor)
-        print(self._hitbox_shrink_x_factor, self._hitbox_shrink_y_factor)
 
         self._hitbox_rect = self.rect.copy().inflate(-self.rect.width * self._hitbox_shrink_x_factor,  #make hitbox rect slightly smaller than image rect
                                                     -self.rect.height * self._hitbox_shrink_y_factor)
@@ -46,15 +46,21 @@ class PrimordialSprite(pygame.sprite.Sprite):
         self._idle_sprite_idx  = 0
         self._sprite_idx       = 0
         self._frame_idx        = 0
-        self._sprites_sequence = self._extract_sprite_sequence(scale=3)
 
-    def _extract_sprite_sequence(self, scale=3):
+        self._sprite_scale      = sprite_scale
+        print("Chromakey:",self._sprite_chromakey)
+        self._sprites_sequence = self._extract_sprite_sequence(scale=self._sprite_scale, chromakey=self._sprite_chromakey)
+
+    def _extract_sprite_sequence(self, scale=3, chromakey=BLACK):
         if self.spritesheet:
-            return self.spritesheet.get_all_sprites(scale=scale)
+            return self.spritesheet.get_all_sprites(scale=scale, chromakey=chromakey)
         else:
             return []
         
-
+    @property
+    def hitbox_rect(self):
+        return self._hitbox_rect
+    
     @property
     def frames_per_animation_step(self):
         return self._frames_per_animation_step
--- spritesheet.py
+++ spritesheet.py
@@ -5,9 +5,10 @@ I followed a tutorial used for this video:
 '''
 
 import pygame
+from config import *
 
 class SpriteSheet():
-    def __init__(self, image, single_width=24, single_height=24, num_cols=8, num_rows=1):
+    def __init__(self, image, single_width=24, single_height=24, num_cols=8, num_rows=1, scale=1):
         self.sheet = image
         self.single_width  = single_width
         self.single_height = single_height
@@ -15,12 +16,14 @@ class SpriteSheet():
         self.num_cols = num_cols
         self.num_rows = num_rows
 
-    @staticmethod
-    def load_from_file(spritesheet_file, single_width=24, single_height=24, num_cols=8, num_rows=1):
-        img = pygame.image.load(spritesheet_file).convert_alpha()
-        return SpriteSheet(img, single_width=single_width, single_height=single_height, num_cols=num_cols, num_rows=num_rows)
+        self.scale = scale
 
-    def get_image(self, frame_pos, width=None, height=None, scale=1, chromakey=(0,0,0)):
+    @staticmethod
+    def load_from_file(spritesheet_file, single_width=24, single_height=24, num_cols=8, num_rows=1, scale=1):
+        img = pygame.image.load(spritesheet_file).convert_alpha()
+        return SpriteSheet(img, single_width=single_width, single_height=single_height, num_cols=num_cols, num_rows=num_rows, scale=scale)
+
+    def get_image(self, frame_pos, width=None, height=None, scale=None, chromakey=(0,0,0)):
         if width is None:
             width = self.single_width
         if height is None:
@@ -32,20 +35,25 @@ class SpriteSheet():
         elif isinstance(frame_pos, int):
             frame_col = frame_pos
 
+
+        if scale is None:
+            scale = self.scale   #use default scale if it is not provided
+
         img = pygame.Surface((width, height)).convert_alpha()
         img.blit(self.sheet, (0,0), ((frame_col * width), (frame_row * height), width, height))
 
         img = pygame.transform.scale(img, (width * scale, height * scale))
+        print("Setting Chromakey to:", chromakey)
         img.set_colorkey(chromakey)
 
         return img
     
-    def get_all_sprites(self, scale=1) -> list:
+    def get_all_sprites(self, scale=1, chromakey=BLACK) -> list:
         """Returns a list of all sprites in spritesheet, based on self.num_cols x self.num_rows"""
         sprites_list = []
         for col in range(self.num_cols):
             for row in range(self.num_rows):
-                img = self.get_image((col,row), scale=scale,)
+                img = self.get_image((col,row), scale=scale, chromakey=chromakey)
                 sprites_list.append(img)
 
         return sprites_list
