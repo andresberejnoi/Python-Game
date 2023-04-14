dfa1bbffcd349bd885a63d4a5c6a63579e6ff7ce
Andres Berejnoi <andresberejnoi@gmail.com>
2023-04-13 22:33:46 -0400

    added new color to global variables

--- config.py
+++ config.py
@@ -5,11 +5,12 @@ SCREEN_WIDTH  = 960
 SCREEN_HEIGHT = 540
 #==========================
 #-- Colors
-WHITE = (255, 255, 255)
-BLACK = (  0,   0,   0)
-RED   = (255,   0,   0)
-GREEN = (  0, 255,   0)
-BLUE  = (  0,   0, 255)
+WHITE       = (255, 255, 255)
+BLACK       = (  0,   0,   0)
+RED         = (255,   0,   0)
+GREEN       = (  0, 255,   0)
+BLUE        = (  0,   0, 255)
+DARKVIOLET  = (148 ,  0, 211)
 
 SCREEN_FILL = (100,200,100)
 
