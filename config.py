8da5268578ed2ad59d7dca00d42edf5cd48bad94
Andres Berejnoi <andresberejnoi@gmail.com>
2023-04-15 12:22:00 -0400

    new variables for 'sounds' folder

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-
+import os
 
 #==========================
 SCREEN_WIDTH  = 960
@@ -7,6 +7,7 @@ SCREEN_HEIGHT = 540
 #-- Colors
 WHITE       = (255, 255, 255)
 BLACK       = (  0,   0,   0)
+GRAY        = (128, 128, 128)
 RED         = (255,   0,   0)
 GREEN       = (  0, 255,   0)
 BLUE        = (  0,   0, 255)
@@ -19,4 +20,6 @@ FPS = 60
 
 #==========================
 #-- Paths
-GRAPHICS_FOLDER = "sprites"
+ASSETS_FOLDER   = "assets"
+GRAPHICS_FOLDER = os.path.join(ASSETS_FOLDER, "sprites")
+SOUNDS_FOLDER   = os.path.join(ASSETS_FOLDER, "sounds")
